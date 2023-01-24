acba12b51e0421cf9aeef1aad36a5273a2f6c21f
UlteaR <56690173+UltearDs@users.noreply.github.com>
2023-01-24 22:08:05 +0100

    Commit ChatGPTest

---
+++ ChatGPT_exec/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+using Newtonsoft.Json;
+
+namespace ChatGPT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string apiKey = "OPENAI_API_KEY";
+            string url = "https://api.openai.com/v1/completions";
+
+            while (true)
+            {
+                Console.WriteLine("___________________");
+                Console.Write("Question : "); // bof
+                string input = Console.ReadLine();
+
+                if (input.ToLower() == "exit")
+                {
+                    break;
+                }
+
+                var request = new
+                {
+                    model = "text-davinci-003",
+                    prompt = input,
+                    temperature = 0.5, // maxValue = 2, 2 = AI works like a drunk guy
+                    max_tokens = 1024,
+                    stop = "",
+                }; // tricky but ok
+
+                using (var client = new HttpClient())
+                {
+                    client.DefaultRequestHeaders.Add("Authorization", $"Bearer {apiKey}");
+
+                    var content = new StringContent(JsonConvert.SerializeObject(request), Encoding.UTF8, "application/json");
+                    var response = client.PostAsync(url, content).Result;
+                    var responseString = response.Content.ReadAsStringAsync().Result;
+
+                    object? v = JsonConvert.DeserializeObject(responseString);
+                    dynamic json = v;
+
+                    Console.WriteLine(json.choices[0].text);
+                }
+            }
+        }
+    }
+}
